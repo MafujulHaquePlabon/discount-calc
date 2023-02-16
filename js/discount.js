6ef3c6c35206ad8f5822382c9342d346f0abf14a
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2023-02-17 00:02:41 +0600

    price discount for product order

---
+++ js/discount.js
@@ -0,0 +1,25 @@
+let submitBtn= document.getElementById('submit-btn');
+document.getElementById("input-field").addEventListener('keyup', function(event){
+    const inputFieldValue= event.target.value;
+    console.log(inputFieldValue)
+    if(inputFieldValue === 'disc30'){
+     submitBtn.removeAttribute('disabled');
+     console.log(submitBtn)
+    }
+    else{
+        submitBtn.setAttribute('disabled',true)
+    }
+});
+
+let count=0;
+submitBtn.addEventListener('click', function(){
+    const bikePriceElement = document.getElementById('price');
+    const bikePriceString= bikePriceElement.innerText;
+    const bikePrice= parseFloat(bikePriceString); 
+    const discount30= (30/100)*bikePrice;
+    const presentBikeTotalPrice=bikePrice-discount30;
+    count +=1;
+    if(count==1){
+        bikePriceElement.innerText=presentBikeTotalPrice;
+    }  
+});
