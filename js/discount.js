94b86d17f4c96737a73abeae2653ee7ec9e5df22
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2023-02-17 00:13:14 +0600

    input field value set empty

--- js/discount.js
+++ js/discount.js
@@ -1,7 +1,7 @@
 let submitBtn= document.getElementById('submit-btn');
 document.getElementById("input-field").addEventListener('keyup', function(event){
     const inputFieldValue= event.target.value;
-    console.log(inputFieldValue)
+   // console.log(inputFieldValue)
     if(inputFieldValue === 'disc30'){
      submitBtn.removeAttribute('disabled');
      console.log(submitBtn)
@@ -22,4 +22,6 @@ submitBtn.addEventListener('click', function(){
     if(count==1){
         bikePriceElement.innerText=presentBikeTotalPrice;
     }  
+const inputFieldValue=document.getElementById('input-field');
+inputFieldValue.value='';
 });
